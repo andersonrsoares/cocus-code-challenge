5c432d79e205e76fc6cfb353ecb261c42a731f71
Anderson Soares <anderson.r.soares@gmail.com>
2020-09-24 15:17:31 -0300

    inject activity and fragment dagger2

---
+++ app/src/main/java/br/com/anderson/cocuscodechallenge/di/AppComponent.kt
@@ -0,0 +1,27 @@
+package br.com.anderson.cocuscodechallenge.di
+
+import android.app.Application
+import br.com.anderson.cocuscodechallenge.ChallengeApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        ActivityBuilder::class
+    ]
+)
+interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(challengeApp: ChallengeApp)
+}
